464fbba1c8c6523c57fd5375d3e7bf70d0513cfa
George Pittarelli <g@gjp.cc>
2018-02-04 22:34:32 -0800

    Cleanup testing junk in showMessage.sh

--- servers/dummy/showMessage.sh
+++ servers/dummy/showMessage.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-function msg () {
-  LEN=$(echo -n $1 | wc -c)
-  echo 'Content-Length: '
-}
+sleep 0.5s
 
-{ sleep 1s;
-  echo IHIHIHIHIH >> asdf.log;
-    echo -n 'Content-Length: 196'$'\r
+echo -n 'Content-Length: 196'$'\r
 \r
-''{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":2,"definitionProvider":true,"documentSymbolProvider":true,"completionProvider":{"resolveProvider":true},"hoverProvider":true}}}'; } &
+''{"jsonrpc":"2.0","id":1,"result":{"capabilities":{"textDocumentSync":2,"definitionProvider":true,"documentSymbolProvider":true,"completionProvider":{"resolveProvider":true},"hoverProvider":true}}}'
 
-
-{ sleep 2s;
-  echo IHIHIHIHIH >> asdf.log;
-    echo -n 'Content-Length: 172'$'\r
+sleep 1s;
+echo -n 'Content-Length: 172'$'\r
 \r
-''{"jsonrpc":"2.0","id":0,"method":"window/showMessageRequest","params":{"type":1,"message":"Choose an option, 1 or 2","actions":[{"label":"Option 1"},{"label":"Option 2"}]}}'; } &
+''{"jsonrpc":"2.0","id":0,"method":"window/showMessageRequest","params":{"type":1,"message":"Choose an option, 1 or 2","actions":[{"title":"Option 1"},{"title":"Option 2"}]}}'
 
+# stay alive so owning process doesn't whine about us dieing
 while read msg; do
-  echo "$msg" >>asdf.log
+  sleep 0.1s;
 done
