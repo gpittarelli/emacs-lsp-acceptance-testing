212d3ec335ab04772a372c930d344cd6e7540bde
George Pittarelli <g@gjp.cc>
2018-04-25 14:00:25 -0700

    Add some simpler rust test fixture code

--- tests/lsp-rust.el
+++ tests/lsp-rust.el
@@ -1,5 +1,6 @@
 ;;; lsp-rust.el --- Testing rust LSP mode -*- lexical-binding: t; -*-
 
 (find-file (file-truename (concat repodir "/tests/fixtures/rust/futures/futures-async-runtime/src/stream.rs")))
+;; (find-file (file-truename (concat repodir "/tests/fixtures/rust/to-and-from/src/main.rs")))
 
 (run-test "rust")
